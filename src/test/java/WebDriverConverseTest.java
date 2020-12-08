0abbb6d9cbd110dcb3757668f49aef1efdaf9973
Alexandra Ogurtsova <ogurtsova-01@mail.ru>
2020-12-08 22:49:07 +0300

    correction

--- src/test/java/WebDriverConverseTest.java
+++ src/test/java/WebDriverConverseTest.java
@@ -20,43 +20,50 @@ public class WebDriverConverseTest {
 
     @Test
     public void AddProductToFavoriteTest(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
         driver.get("https://www.converse.com/shop/p/chuck-taylor-all-star-unisex-high-top-shoe/M9160_040.html?pid=M9006MP&dwvar_M9006MP_size=040&dwvar_M9006MP_color=black&dwvar_M");
-        WebElement closeBlockButton = (new WebDriverWait(driver, 100))
+        WebElement closeBlockButton = (new WebDriverWait(driver, 20))
                 .until(ExpectedConditions.elementToBeClickable(By
                         .xpath("//button[@class='window-modal__close']")));
         closeBlockButton.click();
+        
         WebElement signIn = (new WebDriverWait(driver, 10))
             .until(ExpectedConditions.presenceOfElementLocated(By
                 .xpath("//div[@class='header-utility__item header-utility__item--icon header-user flex display--small-up']")));
         signIn.click();
+        
         WebElement inputLogin = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By
                         .xpath("//input[@class='input-text validate-strict-email tab-hidden  required']")));
         inputLogin.sendKeys("ogurtsova-01@mail.ru");
+        
         WebElement inputPassword = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By
                         .xpath("//input[@class='input-text tab-hidden  required']")));
         inputPassword.sendKeys("20022001_Alog");
+        
         WebElement signInButton = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.elementToBeClickable(By
                         .xpath("//button[@class='button button--primary set--full']")));
         signInButton.click();
-        new WebDriverWait(driver,300).until(ExpectedConditions.presenceOfElementLocated(By
+        
+        new WebDriverWait(driver,30).until(ExpectedConditions.presenceOfElementLocated(By
                 .xpath("//span[text()='Hey, Alexandra']")));
+        
         WebElement addToFavoriteButton = (new WebDriverWait(driver, 20))
                 .until(ExpectedConditions.presenceOfElementLocated(By
                         .xpath("//div[@class='primary-information__favorite invert-theme']")));
         Actions actions = new Actions(driver);
         actions.moveToElement(addToFavoriteButton).click().build().perform();
         addToFavoriteButton.click();
+        
         new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By
                 .cssSelector(".favorite-action__add")));
-        WebElement goToFavoritesButton = (new WebDriverWait(driver, 100))
+        
+        WebElement goToFavoritesButton = (new WebDriverWait(driver, 30))
                 .until(ExpectedConditions.elementToBeClickable(By
                         .xpath("//a[@class='header-favorites__link flex']")));
         goToFavoritesButton.click();
+        
         new WebDriverWait(driver,20).until(ExpectedConditions.presenceOfElementLocated(By
                .xpath("//div[@class='account-head__title flex flex-align-center']")));
 
